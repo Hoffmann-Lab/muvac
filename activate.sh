d2839b989409e42a63bf69f993d416defe2451a3
koriege <koriege@gmail.com>
2023-08-21 19:11:55 +0200

    simplified setup

--- activate.sh
+++ activate.sh
@@ -6,43 +6,5 @@
 [[ "$(uname)" != "Linux" ]] && echo "unsupported operating system" >&2 && return 1
 [[ ${BASH_VERSINFO[0]} -lt 4 || (${BASH_VERSINFO[0]} -eq 4 && ${BASH_VERSINFO[1]} -lt 4) ]] && echo "requieres bash >= v4.4" >&2 && return 1
 
-insdir="$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")"
-toolsdir="$(dirname "$insdir")"
-unset OPTIND activate exitfun help
-while getopts ':i:c:x:a:h' arg; do
-	case $arg in
-		i) toolsdir="$OPTARG";;
-		c) activate="$OPTARG";;
-		x) exitfun="$OPTARG";;
-		h) help=true;;
-		a) shift $((OPTIND-2)); break;;
-		:)	if [[ "$OPTARG" == "a" ]]; then
-				shift $((OPTIND-1))
-				break
-			else
-				echo "argument missing" >&2
-			fi
-			return 1
-			;;
-	esac
-done
-
-if [[ $BASHBONE_DIR ]]; then
-	source "$BASHBONE_DIR/activate.sh" ${help:+-h} -i "${BASHBONE_TOOLSDIR:-$toolsdir}" -c ${activate:-false} -x "$exitfun" -a "$@" || return 1
-else
-	source "$insdir/bashbone/activate.sh" ${help:+-h} -i "$toolsdir" -c ${activate:-false} -x "$exitfun" -a "$@" || return 1
-fi
-
-[[ "$PATH" =~ ^$insdir: ]] || PATH="$insdir:$PATH"
-
-_IFS=$IFS
-IFS=$'\n'
-for f in "$insdir/lib/"*.sh; do
-	BASHBONE_ERROR="file not found $f"
-	[[ -s "$f" ]]
-	source "$f"
-done
-IFS=$_IFS
-
-unset BASHBONE_ERROR
-return 0
+src="$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")"
+source "${BASHBONE_DIR:-$src/bashbone}/activate.sh" -s "$src" "$@" || return 1
--- lib/compile.sh
+++ lib/compile.sh
@@ -1,14 +1,15 @@
 #! /usr/bin/env bash
 # (c) Konstantin Riege
 
-compile::bashbone() {
-	local insdir threads version bashboneversion src="$(dirname "$(readlink -e "$0")")"
+function compile::bashbone(){
+	local insdir threads cfg version bashboneversion src="$(dirname "$(dirname "$(readlink -e "$0")")")"
 	commander::printinfo "installing bashbone"
-	compile::_parse -r insdir -s threads "$@"
+	compile::_parse -r insdir -s threads -f cfg "$@"
 	source "$src/lib/version.sh"
 	rm -rf "$insdir/bashbone-$version"
 	mkdir -p "$insdir/bashbone-$version"
 	cp -r "$src"/* "$insdir/bashbone-$version"
+	rm -f "$insdir/bashbone-$version/scripts/"+(setup|test).sh
 	mkdir -p "$insdir/latest"
 	ln -sfn "$insdir/bashbone-$version" "$insdir/latest/bashbone"
 
@@ -16,18 +17,14 @@ compile::bashbone() {
 	src="$(dirname "$src")"
 
 	commander::printinfo "installing muvac"
-	compile::_parse -r insdir -s threads "$@"
 	source "$src/lib/version.sh"
 	rm -rf "$insdir/muvac-$version"
 	mkdir -p "$insdir/muvac-$version"
 	cp -r "$src"/!(bashbone|setup*) "$insdir/muvac-$version"
+	rm -f "$insdir/muvac-$version/scripts/"+(setup|test).sh
 	mkdir -p "$insdir/latest"
 	ln -sfn "$insdir/muvac-$version" "$insdir/latest/muvac"
 	ln -sfn "$insdir/bashbone-$bashboneversion" "$insdir/muvac-$version/bashbone"
 
 	return 0
-}
-
-compile::muvac() {
-	compile::bashbone
 }
