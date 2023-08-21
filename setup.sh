8f02e29101b8b3184a5e6726326d6414908874f1
koriege <koriege@gmail.com>
2023-08-21 19:12:25 +0200

    moved

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-# (c) Konstantin Riege
-
-src="$(readlink -e "$(dirname "$0")")"
-[[ $# -eq 0 ]] && {
-	exec "$src/bashbone/setup.sh" -h
-} || {
-	exec "$src/bashbone/setup.sh" -s "$src/lib/compile.sh" "$@"
-}
